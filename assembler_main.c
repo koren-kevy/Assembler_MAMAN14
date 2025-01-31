841434526c894fa48f6d43305f5c71271ddfa57e
koren-kevy <korenlevy29@gmail.com>
2025-01-31 17:53:39 +0200

    Update assembler_main.c

--- assembler_main.c
+++ assembler_main.c
@@ -1,10 +1,10 @@
 #include "assembler_definitions.h"
 #include "pre_and_passages.h"
-#include "error_handler.h"
 #include "Utility.h"
 
 int main(int argc, char *argv[])
 {
+    int i;
     Assembler_Table *table = NULL;
 
     table = my_malloc(sizeof(Assembler_Table));
@@ -12,10 +12,16 @@ int main(int argc, char *argv[])
     table->command_head = NULL;
     table->entry_head = NULL;
     table->extern_head = NULL;
+    table->instructions_head = NULL;
     table->label_head = NULL;
     table->macro_head = NULL;
 
-    pre_assembler(&table, argv[1]);
+    for(i = 1; i < argc; i++)
+    {
+        pre_assembler(&table, argv[i]);
+    }
+
+    free(table);
 
     return 0;
 }
