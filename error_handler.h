1d5fb29b5dc2474875994c609b8c8bde445fa06d
koren-kevy <korenlevy29@gmail.com>
2025-01-31 18:02:30 +0200

    Update error_handler.h

--- error_handler.h
+++ error_handler.h
@@ -1,7 +1,12 @@
 #ifndef ERROR_HANDLER_H
 #define ERROR_HANDLER_H
 
-#define no_error 999
-#define error -999
+#define no_error 1
+#define error 0
+
+int check_macro_name_for_instruction(char *line);
+int check_macro_name_for_register(char *line);
+int check_macro_line(char *line);
+int check_for_errors(char *line);
 
 #endif
