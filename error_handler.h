b4d114e0c36d547b962e484fc2d85e6a92ccc4f4
koren-kevy <korenlevy29@gmail.com>
2025-02-01 15:47:43 +0200

    Update error_handler.h

--- error_handler.h
+++ error_handler.h
@@ -4,8 +4,8 @@
 #define no_error 1
 #define error 0
 
-int check_macro_name_for_instruction(char *line, int count);
-int check_macro_name_for_register(char *line, int count);
+int check_macro_name_for_instruction(char *macro_name, int count);
+int check_macro_name_for_register(char *macro_name, int count);
 int check_macro_line(char *line, int count, char *macro_name);
 
 #endif
