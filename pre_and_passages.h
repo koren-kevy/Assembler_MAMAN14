6d3e0c32cef982fb1bf74ea0899ac488b48b2914
koren-kevy <korenlevy29@gmail.com>
2025-02-08 18:52:00 +0200

    Update pre_and_passages.h

--- pre_and_passages.h
+++ pre_and_passages.h
@@ -9,4 +9,8 @@ Macro_List *find_macro(Macro_List *list, char *macro_name);
 int type_line(char *line, Macro_List *list);
 void pre_assembler(Assembler_Table **table_head, char *file_name);
 
+
+void add_label(Label_List **list, char *label_name, int address);
+char *get_lable(char *line, int line_count);
+
 #endif
