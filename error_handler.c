4cca3962f6e5c630abf6d0f5328f978b9607225c
koren-kevy <korenlevy29@gmail.com>
2025-02-22 18:41:38 +0200

    Update error_handler.c

--- error_handler.c
+++ error_handler.c
@@ -1,6 +1,7 @@
 #include "assembler_definitions.h"
 #include "Utility.h"
 #include "error_handler.h"
+#include "pre_and_passages.h"
 
 /*
  * We will build functions that check errors,
@@ -16,29 +17,68 @@ int instruction_functs[] = {0, 0, 1, 2, 0, 1, 2, 3, 4, 1, 2, 3, 0, 0, 0, 0};
 
 char *register_names[] = {"r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7"};
 
+
+int check_space_and_colon(char *line, char *name, int count, int stage)
+{
+    char character = line[strlen(name)];
+    if(character != ':' && stage == FIRST_PASS)
+    {
+        printf("After label definition in line %d, a colon must follow. \n", count);
+        return error;
+    }
+    character = line[strlen(name+1)];
+    if(character != ' ')
+    {
+        if(stage == PRE_PROC)
+        {
+            printf("After macro definition in line %d, a space most follow. \n", count);
+        }
+        else if(stage == FIRST_PASS)
+        {
+            printf("After label definition in line %d, a space most follow. \n", count);
+        }
+        return error;
+    }
+    return no_error;
+}
+
 /* This function already gets a line of a macro definition. */
-int check_macro_name_for_instruction(char *macro_name, int count)
+int check_name_for_instruction(char *name, int count, int stage)
 {
     int i = 0;
     for(; i < TOTAL_INSTRUCTIONS; i++)
     {
-        if(strcmp(instruction_names[i], macro_name) == 0)
+        if(strcmp(instruction_names[i], name) == 0)
         {
-            printf("Macro name is an instruction command in line %d. \n", count);
+            if(stage == PRE_PROC)
+            {
+                printf("Macro name in line %d, is an instruction command. \n", count);
+            }
+            else if(stage == FIRST_PASS)
+            {
+                printf("Label name in line %d, is an instruction command. \n", count);
+            }
             return error;
         }
     }
     return no_error;
 }
 
-int check_macro_name_for_register(char *macro_name, int count)
+int check_name_for_register(char *name, int count, int stage)
 {
     int i = 0;
     for(; i < TOTAL_REGISTERS; i++)
     {
-        if(strcmp(register_names[i], macro_name) == 0)
+        if(strcmp(register_names[i], name) == 0)
         {
-            printf("Macro name is a register name in line %d. \n", count);
+            if(stage == PRE_PROC)
+            {
+                printf("Macro name in line %d, is a register name. \n", count);
+            }
+            else if(stage == FIRST_PASS)
+            {
+                printf("Label name in line %d, is a register name. \n", count);
+            }
             return error;
         }
     }
@@ -60,7 +100,7 @@ int check_macro_line(char *line, int count, char *macro_name)
         {
             if(line[i] != ' ' || line[i] != '\t')
             {
-                printf("In macro ending at line %d are extra charecters. \n", count);
+                printf("In macro ending at line %d, are extra charecters. \n", count);
                 return error;
             }
         }
@@ -83,4 +123,69 @@ int check_macro_line(char *line, int count, char *macro_name)
         }
     }
     return no_error;
+}
+
+int check_label_name(char *name, int count)
+{
+    if(!isalpha(*name))
+    {
+        printf("Label name in line %d, is not a valid label name. \n", count);
+        return error;
+    }
+    return no_error;
+}
+
+int check_for_same_label(Label_List *list, char *name, int count)
+{
+    Label_List *result = find_label(list, name);
+    if(result == NULL)
+    {
+        return no_error;
+    }
+    printf("Label name in line %d, is already a different label. \n", count);
+    return error;
+}
+
+int check_data_line(char *line, int count, char *label)
+{
+    int expecting_num = TRUE;
+    int is_num = FALSE;
+
+    line += strlen(label);
+    if(strncmp(line, ".data", strlen(".data")) != 0)
+    {
+        printf("In data instruction at line %d, there is no .data instruction. \n", count);
+        return error;
+    }
+    while(*line != '\0')
+    {
+        if(expecting_num)
+        {
+            if(*line == '+' || *line == '-' || isdigit(*line))
+            {
+                is_num = TRUE;
+                expecting_num = FALSE;
+            }
+            else
+            {
+                printf("In data instruction at line %d, there isn't a number defined. \n", count);
+                return error;
+            }
+        }
+        else
+        {
+            if(*line == ',')
+            {
+                expecting_num = TRUE;
+            }
+            else if(!isdigit(*line))
+            {
+                printf("In data instruction at line %d,"
+                       " there is an undefined character. \n", count);
+                return error;
+            }
+        }
+        line++;
+    }
+    return !expecting_num && is_num;
 }
