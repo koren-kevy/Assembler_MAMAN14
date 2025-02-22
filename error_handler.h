4d4f45547be80d821bd0afc33372e2427cf513da
koren-kevy <korenlevy29@gmail.com>
2025-02-22 18:41:58 +0200

    Update error_handler.h

--- error_handler.h
+++ error_handler.h
@@ -4,9 +4,18 @@
 #define no_error 1
 #define error 0
 
+int check_space_and_colon(char *line, char *name, int count, int stage);
 
-int check_macro_name_for_instruction(char *macro_name, int count);
-int check_macro_name_for_register(char *macro_name, int count);
+int check_name_for_instruction(char *name, int count, int stage);
+
+int check_name_for_register(char *name, int count, int stage);
+
 int check_macro_line(char *line, int count, char *macro_name);
+
+int check_label_name(char *name, int count);
+
+int check_for_same_label(Label_List *list, char *name, int count);
+
+int check_data_line(char *line, int count, char *label);
 
 #endif
