c568c077f54c339c521c5ab2ba1ad0bbdfeabca9
koren-kevy <korenlevy29@gmail.com>
2025-02-22 18:41:03 +0200

    Update assembler_main.c

--- assembler_main.c
+++ assembler_main.c
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[])
 {
     int i;
+    char *file_name, *file_with_as;
     Assembler_Table *table = NULL;
 
     table = my_malloc(sizeof(Assembler_Table));
@@ -18,7 +19,9 @@ int main(int argc, char *argv[])
 
     for(i = 1; i < argc; i++)
     {
-        pre_assembler(&table, argv[i]);
+        file_name = argv[i];
+        file_with_as = edit_file_name(file_name, ".as");
+        pre_assembler(&table, file_name, file_with_as);
     }
 
     free(table);
